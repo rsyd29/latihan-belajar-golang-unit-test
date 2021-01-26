1dc5d300fb78053505edd8da7c6b6fa4c9c55648
rsyd29 <brasyid15@gmail.com>
2021-01-26 22:07:15 +0700

    Menggagalkan Unit Test Menggunakan Assertion (assert vs require)

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,6 +2,8 @@ package helper
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -56,3 +58,36 @@ func TestHelloWorldRasyid(t *testing.T) {
 Lebih disarankan menggunakan Error() ataupun Fatal() dibandingkan dengan Fail() dan
 FailNow() kenapa karena kita bisa menambahkan informasi kenapa unit test kita gagal
  */
+
+// Belajar Assertion (dengan menggunakan library testify)
+/**
+Assert vs Require
+1. Testify menyediakan dua package untuk assertion, yaitu assert dan require
+2. Saat kita menggunakan assert, jika pengecekkan gagal, maka assert akan memanggil
+Fail(), artinya eksekusi unit test akan tetap dilanjutkan
+3. Sedangkan jika kita menggunakan require, jika pengecekkan gagal, maka require akan
+memanggil FailNow(), artinya eksekusi unit test tidak akan dilanjutkan
+*/
+func TestHelloWorldAssertion (t *testing.T) {
+	result := HelloWorld("Budiman")
+	// Parameter Equal berisikan testing, expected, actual, msgAndArgs
+	// testing itu t
+	// expected hasil yang akan diharapkan
+	// actual -> result atau hasil yang dijalankan
+	// isi pesan apabila gagal
+	assert.Equal(t, "Hello Budiman", result, "Result must be 'Hello Budiman'")
+	// apabila gagal dia akan memanggil Func Fail() yang ada pada t testing
+	// dia akan memanggil func Fail() maka kode program dibawah tetap bisa dijalankan
+	fmt.Println("TestHelloWorld with Assertion is Done")
+	// apabila gagal ini menariknya menggunakan testify
+	// informasinya lebih detail
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Rasyid")
+	require.Equal(t, "Hello Rasyid", result, "Result must be 'Hello Rasyid'")
+	// setelah kode program diatas dijalankan maka kode dibawah tidak akan dijalankan
+	// Kenapa? karena require itu memanggil FailNow()
+	fmt.Println("TestHelloWorld with Require is Done")
+}
+// NOTE: Jadi jangan pakai if else lagi, lebih baik pakai library assertion saja
